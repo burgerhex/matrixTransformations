9e8e84f4055e66ce970fb806e11b5b5d78532f90
burgerhex <aviv.shai@gmail.com>
2019-03-25 17:54:19 -0700

    Update with working example

--- src/Main.java
+++ src/Main.java
@@ -14,27 +14,15 @@ public class Main {
      */
     public static void main(String[] args) {
 
-//        MatrixTransformation id   = MatrixTransformation.IDENTITY;
-//        MatrixTransformation rx   = MatrixTransformation.REFLECT_OVER_X;
-//        MatrixTransformation ry   = MatrixTransformation.REFLECT_OVER_Y;
-//        MatrixTransformation R180 = MatrixTransformation.ROTATE_180;
-//        Rotation R90 = new Rotation(Math.PI);
-//
-//        print("id:\n" + id);
-//        print("rx:\n" + rx);
-//        print("ry:\n" + ry);
-//        print("rx * ry:\n" + ry.compose(rx));
-//        print("R180:\n" + R180);
-//        print("R180 eq rx * ry? " + ry.compose(rx).equals(R180));
-
-//        Rotation R73 =  new Rotation(Math.PI / 5);
-//        Rotation R107 = new Rotation(9 * Math.PI / 5);
-//        print(R73.compose(R107));
+        Rotation R73 =  new Rotation(Math.PI / 5);
+        Rotation R107 = new Rotation(9 * Math.PI / 5);
+        print(R73.compose(R107));
 
         print(MatrixTransformation.composeAll(new MatrixTransformation[] {
-                new MatrixTransformation(1, 0, 2, 1),
-                new MatrixTransformation(2, 0, 0, 2),
-                new MatrixTransformation(3, 0, 1, 2),
+                new ShearX(5),
+                new StretchY(7),
+                new MatrixTransformation(6, 1, 9, 5),
+                new Rotation(1)
         }));
     }
 
